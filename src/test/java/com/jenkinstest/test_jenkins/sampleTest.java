8a22ea514b05fee880ed497149345d88670b77df
mannumukhi <manasadende15@gmail.com>
2019-01-24 11:57:57 +0530

    example for jenkins maven plugin

---
+++ src/test/java/com/jenkinstest/test_jenkins/sampleTest.java
@@ -0,0 +1,17 @@
+package com.jenkinstest.test_jenkins;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class sampleTest 
+{
+
+	
+	@Test
+	public void exampleTest() 
+	{
+		int a=1, b=2;
+		assertTrue(a + b == 3);
+	}
+}
